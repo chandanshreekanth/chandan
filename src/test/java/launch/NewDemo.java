ab345cf27123c28c2907bcabeda61f9369a66a69
Arvin <Arvin@Ravanth>
2022-03-14 10:47:23 +0530

    1st commit

---
+++ src/test/java/launch/NewDemo.java
@@ -0,0 +1,16 @@
+package launch;
+
+import org.testng.annotations.Test;
+
+public class NewDemo
+{
+@Test
+public void demo()
+
+{
+	System.out.println("hiiiiiiiiiii");
+	System.out.println("hiiiiiiiiiii");
+	System.out.println("hiiiiiiiiiii");
+	System.out.println("hiiiiiiiiiii");
+}
+}
